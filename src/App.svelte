8c39ce522de64dd27de9753e3eea3335a7f4bc0c
Sheila Strahan <smstrahan@gmail.com>
2022-08-09 17:07:20 -0600

    svelte tutorial #19

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,14 @@
 <script>
-	import Header from './component.header.svelte';
+	import Header from './components/Header.svelte';
+	import Footer from './components/Footer.svelte';
+
 </script>
 
 <Header />
 <main>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	</p>
+
 </main>
+<Footer />
 
 <style>
 main {
---
+++ src/components/Footer.svelte
@@ -0,0 +1,20 @@
+<footer>
+    <div class="copyright">
+        Copyright 2020 PollNinja
+    </div>
+</footer>
+
+<style>
+    footer {
+        padding: 4-px;
+        text-align: center;
+    }
+
+    .copyright {
+        color: #aaa;
+        font-size: 14px;
+        display: inline-block;
+        padding: 20px;
+        border-top: 1px solid #ddd;
+    }
+</style>
