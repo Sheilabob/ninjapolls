269e2e646389fc1385d09cf1951a493cf55ab5c4
Sheila Strahan <smstrahan@gmail.com>
2022-09-06 18:16:50 -0600

    svelte lesson 30

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,16 +1,30 @@
 <script>
+    // import { onMount, onDestroy } from 'svelte';
+
     import PollStore from '../stores/PollStore';
     import PollDetails from "./PollDetails.svelte";
 
     export let polls =[];
 
-    PollStore.subscribe(data => {
-        polls = data;
-    })
+    // const unsub = PollStore.subscribe(data => {
+    //     polls = data;
+    // });
+
+    // // onMount(() => {
+    // //     //maybe get data from a db
+    // //     console.log('component mounted');
+    // // });
+
+    // // onDestroy
+    // onDestroy(() => {
+    //     //unsub from store
+    //     console.log("component destroyed");
+    //     unsub();
+    // });
 </script>
 
 <div class="poll-list">
-    {#each polls as poll (poll.id)}
+    {#each $PollStore as poll (poll.id)}
         <div>
             <PollDetails {poll} on:vote />    
         </div>
